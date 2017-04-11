7ec9acb29a1594283b04a3f3cd66054a48241c5a
clxhope <changluxin@gmail.com>
2017-04-11 23:13:03 +0800

    实作index和show的action

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,43 @@
+class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.save
+    redirect_to jobs_path
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to jobs_path
+    flash[:notice] = "Update success"
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Job deleted"
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit[:title, :description]
+  end
+end
