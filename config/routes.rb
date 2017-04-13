7bf208e4dafc50f2777eccbe403a7c2466b12bbc
clxhope <changluxin@gmail.com>
2017-04-13 14:42:55 +0800

    设置admin的routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jobs
+
+  namespace :admin do
+    resources :jobs
+  end
 end
