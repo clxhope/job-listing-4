c5e896e0fc837d8b3a00c26a2632f76b50cecc3e
clxhope <changluxin@gmail.com>
2017-04-16 12:46:22 +0800

    管理员可以在后台看到职缺下的所有简历并下载

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,5 @@
 class Resume < ApplicationRecord
+  scope :recent, -> { order("created_at DESC")}
   mount_uploader :attachment, AttachmentUploader
   validates :content, presence: true
   belongs_to :job
