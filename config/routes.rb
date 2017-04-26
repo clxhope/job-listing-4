82b37f9d3bf17880550cef106f88852780303d22
clxhope <changluxin@gmail.com>
2017-04-26 20:44:20 +0800

    implement search function

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jobs do
+    collection do
+      get :search
+    end
     resources :resumes
   end
 
